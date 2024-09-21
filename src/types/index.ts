b36737fbd48531987d066ce64d8cb3220abf2f93
Ulyana Kulbida <unikub@yandex.ru>
2024-09-21 16:46:28 +0600

    updated files README.md & index.ts\types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,62 +1,70 @@
-// Главная страница
-
-export type PaymentMethod = 'online' | 'offline';
 
 // Отображение на странице
-
 export interface IPage {
   catalog: HTMLElement[];   // контейнер c  карточками товаров
   counterr: number;         // счетчик товаров в корзине
   locked: boolean ;         //прокрутки страницы
 };
 
-//Карточка Продукта
-
+// Продукт
 export interface IProduct {
-    id: string;
-    image: string;
-    name: string;
-    description: string;
-    category: string;
-    prise: number | null;
-    inBasket: boolean;
+  id: string;
+  image: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number | null;
 };
 
-// Корзина
-
-export interface IBasket{
-  items: string[];    //массив строк с товарами
-  total: number;      //общая стоимость товаров
-};
-
-//Форма вида оплаты и адреса
-
-interface IOrderForm {
-  payment: PaymentMethod;
-  address: string;
-}
-
-//Данные заказа
-
+// Заказ
 export interface IOrder {
   payment: PaymentMethod;
   email: string;
   phone: string;
   address: string;
-  total: number | null;
-  items: string[];
+  priceTotal: number | null;
+  itemsCard: IProduct[];
 };
 
-// Форма контактных данных
+export type PaymentMethod = 'online' | 'offline'; // Способ оплаты
+export type TProductCatalog = Pick<IProduct, 'category' | 'name' | 'image' | 'price'>;  // Каталог продуктов главного экрана
+export type TProductModalCard = Pick<IProduct, 'category' | 'name' | 'description' | 'image' | 'price'>;  // Попап Карточка продукта
+export type TOrderModalBasket = Pick<IProduct & IOrder, 'name' | 'price' | 'priceTotal'>; // Попап Данные заказа в корзине
+export type TOrderFormPayment = Pick<IOrder, 'payment' | 'address'>;  // Форма вида оплаты и адреса доставки
+export type TOrderFormContacts = Pick<IOrder, 'email' | 'phone'>; // Форма контактных данных
+export type TOrderModalSuccess = Pick<IOrder, 'priceTotal'>;  // Попап успешного заказа
 
-interface IContactsForm {
+// Массив продуктов
+export interface IProductData {
+  products: IProduct[];
+  preview: string | null;
+  getCardsProduct: IProduct[];
+  getCardProduct(idCard: string): IProduct;
+};
+
+// Массив данных в корзине
+export interface IBasketData {
+  products: IProduct[];
+  addProduct(card: IProduct): void;
+  deleteProduct(idCard: string): void;
+  inBasket(idCard: string): boolean;
+  getProducts(): IProduct;
+  clearBasket(): void;
+};
+
+// Массив вида оплаты и контактных данных
+export interface IOrderData {
+  payment: PaymentMethod;
+  address: string;
   email: string;
   phone: string;
-}
+  setOrderPayment(userData: TOrderFormPayment): void;
+  setOrderContacts(userData: TOrderFormContacts): void;
+  checkOrderPaymentValidation(): boolean;
+  checkOrderContactsValidation(): boolean;
+};
 
-// Форма успешного заказа
-
-export interface IOrderResult {
-  id: string;
-  total: number;
+//События клика 
+export interface ICardActions {
+  onClick(event: MouseEvent): void;
 }
